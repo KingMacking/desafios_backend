77bee8638a3fde139a74bf96562f93faef2f018c
Matias Atzori <KingMacking>
2023-02-04 05:20:39 -0300

    Desafio 4 terminado

---
+++ Desafio_4/src/public/index.js
@@ -0,0 +1,41 @@
+const socketClient = io()
+
+const productForm = document.getElementById('productForm');
+
+productForm.addEventListener('submit', (e) => {
+    console.log("submit action");
+    e.preventDefault()
+    const formData = new FormData(productForm)
+
+    fetch('/api', {
+        method: 'POST',
+        body: formData
+    })
+    .then(res => res.json())
+    socketClient.emit('addProduct')
+})
+
+socketClient.on('fetchProducts', () => {
+    fetch('/api', {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(products => {
+        console.log(products);
+        let realTimeProductList = document.getElementById('realTimeProductList')
+        console.log(realTimeProductList);
+        let productList = products.map(prod => {
+            return `<h3>${prod.title}</h3>
+                    <p>${prod.category}</p>
+                    <p>Descripcion:${prod.description}</p>
+                    <p>Precio: $ ${prod.price}</p>
+                    <p>Codigo: ${prod.code}</p>
+                    <p>Stock: ${prod.stock}</p>
+                    <p>ID: ${prod.id}</p>
+                    <img src=${prod.thumbnail} alt="product image"></img>`
+        }).join(' ')
+        console.log(productList);
+        realTimeProductList.innerHTML = productList
+        console.log(realTimeProductList);
+    })
+})
